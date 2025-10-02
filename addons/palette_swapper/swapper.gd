17c764ad51512d33cb46fda7a46e8c502332e4f9
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-10-02 23:25:45 +0100

    made easy toolbar

--- addons/palette_swapper/swapper.gd
+++ addons/palette_swapper/swapper.gd
@@ -1,12 +1,21 @@
 @tool
 extends EditorPlugin
 
+var toolbar
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
-
+	
+	toolbar = preload("res://addons/palette_swapper/test.tscn").instantiate()
+	
+	#add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, toolbar)
+	#add_control_to_bottom_panel(toolbar, "Swapper")
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BL, toolbar)
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	#remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, toolbar)
+	#remove_control_from_bottom_panel(toolbar)
+	remove_control_from_docks(toolbar)
+	
+	toolbar.free()
