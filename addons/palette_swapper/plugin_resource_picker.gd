c17581e7d73e9d9cd7fd0c84d7e0c4504b1eec77
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-10-04 09:50:30 +0100

    preview and shader work in the editor

--- addons/palette_swapper/plugin_resource_picker.gd
+++ addons/palette_swapper/plugin_resource_picker.gd
@@ -3,11 +3,13 @@ extends EditorResourcePicker
 
 class_name AddonResourcePicker
 
+signal texture_selected(tex: Texture2D)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	base_type = "Texture"
+	base_type = "Texture2D"
+	resource_changed.connect(_on_resource_changed)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_resource_changed(res: Resource):
+	if res is Texture2D:
+		emit_signal("texture_selected", res)
--- addons/palette_swapper/toolpanel.gd
+++ addons/palette_swapper/toolpanel.gd
@@ -1,13 +1,42 @@
 @tool
 extends VBoxContainer
 
-const TEST_SWAP = preload("res://testing/test_swap.gdshader")
+@onready var picker: AddonResourcePicker = $HBoxContainer3/AddonResourcePicker
+@onready var sprite: Sprite2D = $SubViewport/Sprite2D
+@onready var viewport: SubViewport = $SubViewport
+
+var texture
+var shader = preload("res://testing/test_swap.gdshader")
+var mat
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	picker.texture_selected.connect(_on_texture_selected)
+	
+	mat = ShaderMaterial.new()
+	mat.shader = shader
+	sprite.material = mat
+	sprite.texture = texture
+	
+	
+	$TextureRect.texture = viewport.get_texture()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_texture_selected(tex: Texture2D):
+	
+	
+	sprite.texture = tex
+
+func _on_target_color_picker_color_changed(color: Color) -> void:
+	mat.set_shader_parameter("target_color", color)
+
+func _on_replace_color_picker_color_changed(color: Color) -> void:
+	mat.set_shader_parameter("replace_color", color)
+
+func _on_tolerance_value_changed(value: float) -> void:
+	mat.set_shader_parameter("tolerance", value)
